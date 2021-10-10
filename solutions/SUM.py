accf85b3563ed70438798bc6705eb759669c2109
sbx_user1051 <sbx_user1051@169.254.17.133>
2021-10-10 17:39:04 +0000

    Sun Oct 10 17:39:04 UTC 2021

--- solutions/SUM.py
+++ solutions/SUM.py
@@ -2,5 +2,5 @@ class sum_solution:
     def compute(value1: int, value2: int) -> int:
         return value1 + value2
 
-    def sum(value1: int, value2: int) -> int:
-        return sum_solution.compute(value1, value2)
+    # def sum(value1: int, value2: int) -> int:
+    #     return sum_solution.compute(value1, value2)

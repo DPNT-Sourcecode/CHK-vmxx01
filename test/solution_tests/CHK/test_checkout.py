bff59863841f87a2b7e4acaac11579dbea281664
sbx_user1051 <sbx_user1051@169.254.9.253>
2021-10-11 15:06:22 +0000

    Mon Oct 11 15:06:22 UTC 2021

---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,8 @@
+from solutions.CHK.checkout_solution import checkout
+
+
+class TestCheckout():
+    def test_illegal_input(self):
+        assert checkout("") == -1
+        assert checkout("E") == -1
+        assert checkout("123") == -1

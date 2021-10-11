081ecef1558fa47da78620e4ff195f26ad3acb58
sbx_user1051 <sbx_user1051@169.254.9.253>
2021-10-11 17:37:24 +0000

    Mon Oct 11 17:37:24 UTC 2021

--- lib/solutions/CHK/price_table.py
+++ lib/solutions/CHK/price_table.py
@@ -4,11 +4,13 @@ PRICE_TABLE = {
         "offers": [
             {
                 "target_sku": "A",
+                "target_units": 3,
                 "source_units": 3,
                 "offer_discount": 20
             },
             {
                 "target_sku": "A",
+                "target_units": 5,
                 "source_units": 5,
                 "offer_discount": 50
             }, 
@@ -19,6 +21,7 @@ PRICE_TABLE = {
         "offers": [
             {
                 "target_sku": "B",
+                "target_units": 2,
                 "source_units": 2,
                 "offer_discount": 15
             },
@@ -35,6 +38,7 @@ PRICE_TABLE = {
         "offers": [
             {
                 "target_sku": "B",
+                "target_units": 1,
                 "source_units": 2,
                 "offer_discount": 30
             }

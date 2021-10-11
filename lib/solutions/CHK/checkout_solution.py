3c99e85eb68156fca59ccd31c7aa5318c2f340cf
sbx_user1051 <sbx_user1051@169.254.9.253>
2021-10-11 15:18:22 +0000

    Mon Oct 11 15:18:22 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,29 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+PRICE_TABLE = {
+    "A": {
+        "price": 50,
+        "offer": {
+            "offer_unit": 3,
+            "offer_price": 30
+        }
+    },
+    "B": {
+        "price": 30,
+        "offer": {
+            "offer_unit": 2,
+            "offer_price": 15
+        }
+    },
+    "C": {
+        "price": 20
+    },
+    "D": {
+        "price": 15
+    }
+}
+
+def checkout(skus: str):
+    for sku in skus:
+        print(sku)
+
